8793774cdb688bdccfa02480fcb7d6894f92e09b
David Thomson <31463870+MegaBoy117@users.noreply.github.com>
2023-03-04 18:05:16 +0000

    Changed name to lowercase

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ async def hello(ctx):
     await ctx.send('Hello!')
 
 
-@bot.tree.command(name="Challenge", description="Challenge a user to a duel. Has to be on the same level as you or above")
+@bot.tree.command(name="challenge", description="Challenge a user to a duel. Has to be on the same level as you or above")
 async def challenge(ctx, user: discord.User):
     await ctx.send('You have challenged: ' + user.mention)
 
