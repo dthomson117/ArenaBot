ec2e7e12ad65fd89e1dd3f63619dfe85a5576c5a
David Thomson <31463870+MegaBoy117@users.noreply.github.com>
2023-03-04 18:25:07 +0000

    Added test command to add 2 numbers

--- main.py
+++ main.py
@@ -18,10 +18,23 @@ bot = commands.Bot(
 async def hello(ctx):
     await ctx.send('Hello!')
 
-
-@bot.tree.command(name="challenge", description="Challenge a user to a duel. Has to be on the same level as you or above")
+"""
+@bot.tree.command(name="challenge",
+                  description="Challenge a user to a duel. Has to be on the same level as you or above")
 async def challenge(ctx, interaction: discord.Interaction):
     await ctx.send('You have challenged: ' + interaction.user.mention)
+
+"""
+
+
+@bot.tree.command()
+@app_commands.describe(
+    first_value='The first value you want to add something to',
+    second_value='The value you want to add to the first value',
+)
+async def add(interaction: discord.Interaction, first_value: int, second_value: int):
+    """Adds two numbers together."""
+    await interaction.response.send_message(f'{first_value} + {second_value} = {first_value + second_value}')
 
 
 bot.run(
