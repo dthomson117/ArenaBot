ba7c039b4d88516b923b70918dd5938bf3f50366
David Thomson <31463870+MegaBoy117@users.noreply.github.com>
2023-03-04 18:08:14 +0000

    Created new tree variable from bot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
 
 f = open('token.txt', 'r')
 token = f.read()
@@ -12,13 +13,15 @@ bot = commands.Bot(
     intents=intents
 )
 
+tree = app_commands.CommandTree(bot)
+
 
 @bot.command()
 async def hello(ctx):
     await ctx.send('Hello!')
 
 
-@bot.tree.command(name="challenge", description="Challenge a user to a duel. Has to be on the same level as you or above")
+@tree.command(name="challenge", description="Challenge a user to a duel. Has to be on the same level as you or above")
 async def challenge(ctx, user: discord.User):
     await ctx.send('You have challenged: ' + user.mention)
 
